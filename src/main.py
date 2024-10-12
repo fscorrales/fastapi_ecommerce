c96f4a4002bd41eb0011f447bc6f201debafa385
Fernando Corrales <corrales_fernando@hotmail.com>
2024-10-12 09:10:12 -0300

    connected user's routes to main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
+from .api.routes import api_router
 
 tags_metadata = []
 
 app = FastAPI(title="Final Project API", openapi_tags=tags_metadata)
+
+# Include our API routes
+app.include_router(api_router)
 
 
 @app.get("/", include_in_schema=False)
