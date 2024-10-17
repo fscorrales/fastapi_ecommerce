845b5e9db59ecb133aab3384da96cb5acd2bf770
Fernando Corrales <corrales_fernando@hotmail.com>
2024-10-17 14:41:48 -0300

    add order's model, route and service

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -5,7 +5,9 @@ from fastapi import APIRouter
 from .auth import auth_router
 from .users import users_router
 from .products import products_router
+from .orders import orders_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(users_router)
 api_router.include_router(products_router)
+api_router.include_router(orders_router)
