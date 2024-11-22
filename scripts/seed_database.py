b80d0b9955cfb31ade6f00d9f8389251ab96b7a2
Fernando Corrales <fscorrales@gmail.com>
2024-11-22 08:45:30 -0300

    update seed_database with orders seed

--- scripts/seed_database.py
+++ scripts/seed_database.py
@@ -1,4 +1,4 @@
-from src.api.models import CreateUser, Order, CreateProduct, Category
+from src.api.models import CreateUser, Order, CreateProduct, Category, OrderStatus
 from src.api.services import OrdersService, ProductsService, UsersService
 from src.api.config import UNSPLASH_ACCESS_KEY
 from .get_img_unsplash import get_img_unsplash
@@ -11,7 +11,7 @@ data_dir = os.path.join(current_dir, "sample_data")
 
 # Read JSON from randomuser.me api
 data_file = os.path.join(data_dir, "users.json")
-with open(data_file) as f:
+with open(data_file, encoding='utf-8') as f:
     users = json.load(f)
 
 results = users["results"]
@@ -31,17 +31,9 @@ for result in results:
     result_id = UsersService.create_one(insertion_user).model_dump()["id"]
     (customer_ids if user["role"] == "customer" else seller_ids).append(result_id)
 
-
-# Definición de tipos de productos
-percussion = "Percussion"
-wind = "Wind"
-string = "String"
-keyboard = "Keyboard"
-electronic = "Electronic"
-
 # Read JSON from mockaroo api
 data_file = os.path.join(data_dir, "products.json")
-with open(data_file) as f:
+with open(data_file, encoding='utf-8') as f:
     results = json.load(f)
 
 # Create some products
@@ -73,73 +65,30 @@ for result in results:
     product_ids.append(result_id)
 
 # Create some orders
+order_status = [order.value for order in OrderStatus]
 
-orders = [
-    {
+orders = []
+
+for _ in range(1000):
+    order = {
         "customer_id": random.choice(customer_ids),
-        "status": "completed",
-        "order_products": [
-            {
-                "product_id": random.choice(product_ids),
-                "price": 400,
-                "quantity": 3,
-            },
-            {
-                "product_id": random.choice(product_ids),
-                "price": 500,
-                "quantity": 2,
-            },
-        ],
-    },
-    {
-        "customer_id": random.choice(customer_ids),
-        "status": "shopping",
-        "order_products": [
-            {
-                "product_id": random.choice(product_ids),
-                "price": 600,
-                "quantity": 2,
-            },
-            {
-                "product_id": random.choice(product_ids),
-                "price": 700,
-                "quantity": 5,
-            },
-        ],
-    },
-    {
-        "customer_id": random.choice(customer_ids),
-        "status": "cancelled",
-        "order_products": [
-            {
-                "product_id": random.choice(product_ids),
-                "price": 400,
-                "quantity": 2,
-            },
-            {
-                "product_id": random.choice(product_ids),
-                "price": 600,
-                "quantity": 3,
-            },
-        ],
-    },
-    {
-        "customer_id": random.choice(customer_ids),
-        "status": "completed",
-        "order_products": [
-            {
-                "product_id": random.choice(product_ids),
-                "price": 800,
-                "quantity": 1,
-            },
-            {
-                "product_id": random.choice(product_ids),
-                "price": 900,
-                "quantity": 2,
-            },
-        ],
-    },
-]
+        "status": random.choice(order_status),
+        "order_products": [],
+    }
+
+    num_products = random.randint(1, 5)
+    for _ in range(num_products):
+        product_id = random.choice(product_ids)
+        price = round(random.uniform(0.1, 1000000), 2)
+        quantity = random.randint(1, 3)
+        order["order_products"].append({
+            "product_id": product_id,
+            "price": price,
+            "quantity": quantity,
+        })
+
+    orders.append(order)
+
 
 print("Creating orders...")
 for order in orders:
